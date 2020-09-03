1492f66e89df6c65d1b5c1639deb6ee024e5ecca
wupeiwen <wupeiwen@lecarlink.com>
2020-09-03 16:48:28 +0800

    1

--- src/main/java/com/wpw/demo/DemoApplication.java
+++ src/main/java/com/wpw/demo/DemoApplication.java
@@ -4,13 +4,14 @@ import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class, WebMvcAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(Application.class);
+		SpringApplication application = new SpringApplication(DemoApplication.class);
 		application.addListeners(new ApplicationPidFileWriter());
 		application.run(args);
 	}
